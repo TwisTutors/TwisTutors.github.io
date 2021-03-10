76750d01bf35d315bc4ea5633c576ddeca18b5b7
Sam_Ash11414 <72979934+SamAsh11414@users.noreply.github.com>
2021-03-10 11:29:02 -0500

    added Form functionality, Newsletter and contact

---
+++ assets/js/FormHandler.js
@@ -0,0 +1,11 @@
+console.log('Enjoy this random easter egg from the dev as he struggles with this code because hes tired and doesnt have a brain')
+
+$(document).ready(function(){
+    $('.submit').click(function(){
+
+        var email = $('.email').val()
+        var name = $('.name').val()
+        var subject = $('.subject').val()
+        var message = $('.message').val()
+    })
+})
---
+++ assets/js/NewsLetterFH.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.subscribe').click(function(){
+
+        var email = $('.email').val()
+        var name = $('.name').val()
+        var phone = $('.phone').val()
+    })
+})
