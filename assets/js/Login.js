c8737718f9c3da2857ad39826e9672ce51cb7028
Sam_Ash11414 <72979934+SamAsh11414@users.noreply.github.com>
2021-03-11 21:43:07 -0500

    Fixed Login issue

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -42,7 +42,7 @@ function signIn() {
     var email = document.getElementById("email")
     var password = document.getElementById("password")
 
-        const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+        const promise = auth.signInWithEmailAndPassword(email.value, password.value);
         promise.catch(e => alert(e.message))
 
         alert("Signed In " + email )
